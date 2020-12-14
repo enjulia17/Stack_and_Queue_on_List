d94d4d7f0bbc29d7ecc011b1287cc7bd1a20a2e4
enjulia17 <enjulia@mail.ru>
2020-12-14 18:12:14 +0300

    Latest version

--- include/StackQueue.h
+++ include/StackQueue.h
@@ -1,3 +1,40 @@
 #pragma once
 #include <List.h>
 
+template<class T>
+class TStackOnList
+{
+protected:
+	List<T> Container;
+public:
+	TStackOnList() : Container() {}
+	TStackOnList(TStackOnList& _v) : Container(_v.Container) {}
+
+	bool IsEmpty()
+	{
+		return(Container.IsEmpty());
+	}
+	int GetSize()
+	{
+		return(Container.GetSize());
+	}
+	virtual void push(const T data)
+	{
+		Container.push_front(data);
+	}
+	T pop()
+	{
+		return(Container.pop_front());
+	}
+};
+
+template<class T>
+class TQueueOnList :public TStackOnList<T>
+{
+public:
+	TQueueOnList() :TStackOnList() {}
+	void push(const T data) override
+	{
+		this->Container.push_back(data);
+	}
+};
--- test/test_stackqueue.cpp
+++ test/test_stackqueue.cpp
@@ -1,3 +1,80 @@
 #include "StackQueue.h"
-
 #include <gtest.h>
+
+TEST(TStack, can_create_stack)
+{
+	ASSERT_NO_THROW(TStackOnList<int> A);
+}
+
+TEST(TStack, can_copy_stack)
+{
+	TStackOnList<int> A;
+	A.push(100);
+	ASSERT_NO_THROW(TStackOnList<int> B(A));
+}
+
+TEST(TStack, push)
+{
+	TStackOnList<int> A;
+	A.push(1);
+	A.push(2);
+
+	EXPECT_EQ(2, A.pop());
+}
+
+TEST(TStack, pop)
+{
+	TStackOnList<int> A;
+	A.push(1);
+	A.push(2);
+	A.push(3);
+	A.pop();
+	A.pop();
+
+	EXPECT_EQ(1, A.pop());
+}
+
+TEST(TStack, IsEmpty)
+{
+	TStackOnList<int> A;
+	EXPECT_EQ(1, A.IsEmpty());
+}
+
+
+TEST(TQueue, can_create_queue)
+{
+	ASSERT_NO_THROW(TQueueOnList<int> A);
+}
+
+TEST(TStack, can_copy_queue)
+{
+	TQueueOnList<int> A;
+	A.push(100);
+	ASSERT_NO_THROW(TQueueOnList<int> B(A));
+}
+
+TEST(TQueue, push)
+{
+	TQueueOnList<int> A;
+	A.push(1);
+	A.push(2);
+	EXPECT_EQ(1, A.pop());
+}
+
+TEST(TQueue, pop)
+{
+	TQueueOnList<int> A;
+	A.push(1);
+	A.push(2);
+	A.push(3);
+	A.pop();
+	A.pop();
+
+	EXPECT_EQ(3, A.pop());
+}
+
+TEST(TQueue, IsEmpty)
+{
+	TQueueOnList<int> A;
+	EXPECT_EQ(1, A.IsEmpty());
+}
